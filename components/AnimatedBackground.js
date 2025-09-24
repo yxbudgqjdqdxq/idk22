61bb4e65dd61b6ee178972e2331b5d93ed86d21c
yxbudgqjdqdxq <93042363+yxbudgqjdqdxq@users.noreply.github.com>
2025-09-24 05:50:22 -0700

    Update AnimatedBackground.js

--- components/AnimatedBackground.js
+++ components/AnimatedBackground.js
@@ -1,13 +1,10 @@
 // components/AnimatedBackground.js
 export default function AnimatedBackground() {
   return (
-    <div className="animated-bg" aria-hidden="true" data-animated-bg="true">
-      <div className="gradient-overlay" />
-      <div className="blob blob-1" />
-      <div className="blob blob-2" />
-      <div className="blob blob-3" />
-      <div className="blob blob-4" />
+    <div className="animated-bg">
+      <div className="blob"></div>
+      <div className="blob"></div>
+      <div className="blob"></div>
     </div>
-  );
+  )
 }
-
